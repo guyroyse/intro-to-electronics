59358c473429b0b2b5deeeaa9fcfcac9f616cf4f
Guy Royse <guy@guyroyse.com>
2019-12-26 17:06:28 -0500

    simplify

--- code/onoff/onoff.ino
+++ code/onoff/onoff.ino
@@ -1,6 +1,5 @@
 const int ledPin =  LED_BUILTIN;
 int ledState = LOW;
-bool connected = false;
 
 const long interval = 5000;
 unsigned long previousMillis = 0;
@@ -16,24 +15,8 @@ void loop() {
   unsigned long currentMillis = millis();
 
   if (currentMillis - previousMillis >= interval) {
-
     previousMillis = currentMillis;
-
-    if (Serial.available() >= 3) {
-
-      char buffer[4];
-      Serial.readBytes(buffer, 3);
-      buffer[3] = 0;
-
-      if (strcmp("+++", buffer) == 0) {
-        connected = true;
-      }
-    }
-    
-    if (connected == true) {      
-      Serial.print("*2\r\n$3\r\nGET\r\n$13\r\narduino:pin:13\r\n");
-    }
-    
+    Serial.print("*2\r\n$3\r\nGET\r\n$13\r\narduino:pin:13\r\n");
   }
 }
 
--- code/redpeter-js/app.js
+++ code/redpeter-js/app.js
@@ -2,21 +2,16 @@ const net = require('net')
 
 const SerialPort = require('serialport')
 
-const port = new SerialPort('/dev/cu.usbmodem141301', { baudRate: 9600, autoOpen: false })
+const port = new SerialPort('/dev/cu.usbmodem141301', { baudRate: 9600 })
 port.on('data', data => {
   console.log('Sent:', data.toString().replace("\r", "<CR>").replace("\n", "<LF>"))
   socket.write(data.toString())
 })
 
 const socket = new net.Socket()
-socket.on('data', function(data) {
+socket.on('data', data => {
   console.log('Rcvd:', data.toString().replace("\r", "<CR>").replace("\n", "<LF>"))
-})
-
-port.open(() => {
-  setTimeout(() => {
-    port.write('+++')
-  }, 3000)
+  port.write(data.toString());
 })
 
 socket.connect(6379, 'localhost');
